66ec958d3586176064884c09b6320d7fa39019a6
github username <github email address>
2019-09-25 09:34:25 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,8 +15,9 @@ class Form extends Component {
   state = {
     ...DEFAULT_STATE
   }
+  
 
-  handleSubmit() {
+  handleSubmit = (event) => {
     event.preventDefault()
     document.getElementById("order-form").reset()
     this.props.addOrder(this.state)
@@ -26,9 +27,12 @@ class Form extends Component {
     })
   }
 
-  handleChange() {
+  handleChange = (event) => {
     const itemType = event.target.name
     const item = event.target.value
+
+    console.log(itemType)
+    console.log(item)
 
     !this.state[`${itemType}`].includes(item) ?
       this.setState({
@@ -49,22 +53,22 @@ class Form extends Component {
         <form className="ui form" id="order-form" onSubmit={ this.handleSubmit }>
           <ProteinForm
             protein={ this.state.protein }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <FillingForm
             fillings={ this.state.fillings }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <ToppingForm
             toppings={ this.state.toppings }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <SideForm
             sides={ this.state.sides }
-            handleOnChange={ this.handleChange }
+            handleChange={ this.handleChange }
           />
 
           <br />
